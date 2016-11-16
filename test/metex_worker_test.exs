1eb6ca31738a464a52afadcbba3eb1a642adc88f
SengMing Tan <tansengming@gmail.com>
2016-11-16 17:49:05 -0500

    cleans up test

--- test/metex_worker_test.exs
+++ test/metex_worker_test.exs
@@ -1,19 +1,24 @@
 defmodule Metex.WorkerTest do
   use ExUnit.Case
 
-  test '.start_link' do
-    {status, _pid} = Metex.Worker.start_link([])
-
-    assert status == :ok
+  setup_all do
+    {status, pid} = Metex.Worker.start_link([])
+    {:ok, server_pid: pid, status: status}
   end
 
-  test '.get_temperature' do
-    {:ok, server_pid} = Metex.Worker.start_link([])
-    assert Metex.Worker.get_temperature(server_pid, "Sydney") != nil
+  test '.start_link', state do
+    assert state[:status] == :ok
   end
 
-  test '.get_stats' do
-    {:ok, server_pid} = Metex.Worker.start_link([])
-    assert Metex.Worker.get_stats(server_pid) != nil
+  test '.get_temperature', state do
+    assert Metex.Worker.get_temperature(state[:server_pid], "Sydney") != nil
   end
+
+  test '.get_stats', state do
+    assert Metex.Worker.get_stats(state[:server_pid]) != nil
+  end
+
+  # test '.stop' do
+  #   {:ok, server_pid} = Metex.Worker.start_link([])
+  # end
 end

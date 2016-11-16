c3637f9dc8705b4addadf1e45f91bc14030e0e75
SengMing Tan <tansengming@gmail.com>
2016-11-16 18:03:07 -0500

    uses name instead of PID

--- test/metex_worker_test.exs
+++ test/metex_worker_test.exs
@@ -11,26 +11,26 @@ defmodule Metex.WorkerTest do
     assert state[:status] == :ok
   end
 
-  test '.get_temperature', state do
-    assert Metex.Worker.get_temperature(state[:server_pid], "Sydney") != nil
+  test '.get_temperature' do
+    assert Metex.Worker.get_temperature("Sydney") != nil
   end
 
-  test '.get_stats', state do
-    assert Metex.Worker.get_stats(state[:server_pid]) == %{}
+  test '.get_stats' do
+    assert Metex.Worker.get_stats == %{}
 
-    Metex.Worker.get_temperature(state[:server_pid], "Sydney")
-    assert Metex.Worker.get_stats(state[:server_pid]) == %{"Sydney" => 1}
+    Metex.Worker.get_temperature("Sydney")
+    assert Metex.Worker.get_stats == %{"Sydney" => 1}
 
-    Metex.Worker.get_temperature(state[:server_pid], "Sydney")
-    assert Metex.Worker.get_stats(state[:server_pid]) == %{"Sydney" => 2}
+    Metex.Worker.get_temperature("Sydney")
+    assert Metex.Worker.get_stats == %{"Sydney" => 2}
 
-    Metex.Worker.get_temperature(state[:server_pid], "Singapore")
-    assert Metex.Worker.get_stats(state[:server_pid]) == %{"Sydney" => 2, "Singapore" => 1}
+    Metex.Worker.get_temperature("Singapore")
+    assert Metex.Worker.get_stats == %{"Sydney" => 2, "Singapore" => 1}
   end
 
   test '.stop', state do
     assert true == Process.alive?(state[:server_pid])
-    Metex.Worker.stop(state[:server_pid])
+    Metex.Worker.stop
 
     # can't test because stop is async
     # assert false == Process.alive?(state[:server_pid])

08a06301410fcec7c8bbe606f9654f5b85974568
SengMing Tan <tansengming@gmail.com>
2017-04-27 16:31:37 -0400

    clean up test with import

--- test/metex_worker_test.exs
+++ test/metex_worker_test.exs
@@ -1,8 +1,9 @@
 defmodule Metex.WorkerTest do
   use ExUnit.Case, async: true
+  import Metex.Worker, only: [get_temperature: 1, get_stats: 0, start_link: 1, stop: 0, handle_call: 3]
 
   setup do
-    {status, pid} = Metex.Worker.start_link([])
+    {status, pid} = start_link([])
     # Note: setup expects to return {:ok, state}
     {:ok, server_pid: pid, status: status}
   end
@@ -12,25 +13,25 @@ defmodule Metex.WorkerTest do
   end
 
   test '.get_temperature' do
-    assert Metex.Worker.get_temperature("Sydney") != nil
+    assert get_temperature("Sydney") != nil
   end
 
   test '.get_stats' do
-    assert Metex.Worker.get_stats == %{}
+    assert get_stats() == %{}
 
-    Metex.Worker.get_temperature("Sydney")
-    assert Metex.Worker.get_stats == %{"Sydney" => 1}
+    get_temperature("Sydney")
+    assert get_stats() == %{"Sydney" => 1}
 
-    Metex.Worker.get_temperature("Sydney")
-    assert Metex.Worker.get_stats == %{"Sydney" => 2}
+    get_temperature("Sydney")
+    assert get_stats() == %{"Sydney" => 2}
 
-    Metex.Worker.get_temperature("Singapore")
-    assert Metex.Worker.get_stats == %{"Sydney" => 2, "Singapore" => 1}
+    get_temperature("Singapore")
+    assert get_stats() == %{"Sydney" => 2, "Singapore" => 1}
   end
 
   test '.stop', state do
     assert true == Process.alive?(state[:server_pid])
-    Metex.Worker.stop
+    stop()
 
     # can't test because stop is async
     # assert false == Process.alive?(state[:server_pid])
@@ -39,19 +40,19 @@ defmodule Metex.WorkerTest do
   # these tests do not require the server, compare with the tests that
   # require start link above
   test 'handle get_stats' do
-    {:reply, _stats, stats} = Metex.Worker.handle_call(:get_stats, 1, %{})
+    {:reply, _stats, stats} = handle_call(:get_stats, 1, %{})
     assert stats == %{}
 
-    {:reply, _stats, stats} = Metex.Worker.handle_call(:get_stats, 1, %{"Sydney" => 1})
+    {:reply, _stats, stats} = handle_call(:get_stats, 1, %{"Sydney" => 1})
     assert stats == %{"Sydney" => 1}
 
   end
 
   test 'handle location' do
-    {:reply, _temp, stats} = Metex.Worker.handle_call({:location, "Sydney"}, nil, %{})
+    {:reply, _temp, stats} = handle_call({:location, "Sydney"}, nil, %{})
     assert stats == %{"Sydney" => 1}
 
-    {:reply, _temp, stats} = Metex.Worker.handle_call({:location, "Sydney"}, nil, %{"Sydney" => 1, "Kuala Lumpur" => 1})
+    {:reply, _temp, stats} = handle_call({:location, "Sydney"}, nil, %{"Sydney" => 1, "Kuala Lumpur" => 1})
     assert stats == %{"Sydney" => 2, "Kuala Lumpur" => 1}
   end
 end
